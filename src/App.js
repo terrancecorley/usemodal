a0de29f7697d140639e6b8188871d475d9083fbc
Terrance Corley <tcorley@stepstoneglobal.com>
2019-05-29 15:38:32 -0700

    added base import for alert comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Alert from '@bit/terrance_corley.stepstonedemo.alert';
+import Button from '@bit/terrance_corley.stepstonedemo.alert';
 
 function App() {
     return (
-        <div className="App">
-            hi
-        </div>
+        <>
+            <div className="App">
+                <Alert></Alert>
+            </div>
+            <Button>testtt</Button>
+        </>
     );
 }
 
