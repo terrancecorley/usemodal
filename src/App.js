c0d6ced062b13c9d6498ae7aa2788f9c7b2f14d6
Terrance Corley <tcorley@stepstoneglobal.com>
2019-05-30 11:18:03 -0700

    added bit alert component to project via npm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Alert from '@bit/terrance_corley.stepstonedemo.alert';
-import Button from '@bit/terrance_corley.stepstonedemo.alert';
+import Button from '@bit/terrance_corley.stepstonedemo.button';
+import styled from 'styled-components';
 
-function App() {
-    return (
-        <>
-            <div className="App">
-                <Alert></Alert>
-            </div>
-            <Button>testtt</Button>
-        </>
-    );
+class App extends Component {
+    state = {
+        displayAlert: false
+    }
+
+    showAlert = () => {
+        this.setState({
+            displayAlert: true
+        });
+    }
+
+    hideAlert = () => {
+        this.setState({
+            displayAlert: false
+        });
+    }
+
+    render() {
+        let shouldDisplayAlert = this.state.displayAlert;
+
+        return (
+            <>
+                <div className="App">
+                    <AlertBtn onClick={this.showAlert}>Show alert</AlertBtn>
+                    {shouldDisplayAlert && 
+                        <Alert
+                        title="Alert Title"
+                        onClick={this.hideAlert}
+                        >
+                            This is an alert for StepStone demo purposes.
+                        </Alert>
+                    }
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
+
+const AlertBtn = styled(Button)`
+    margin: 10% auto 0;
+    display: block;
+`;
